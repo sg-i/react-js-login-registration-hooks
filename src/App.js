2163739bbf491ac1f005c0168c0e0117671fbffd
4017 Сгадов И.В <sgadov2vanya@yandex.ru>
2023-06-05 18:25:29 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import React, { useState, useEffect } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-import AuthService from "./services/auth.service";
+import AuthService from './services/auth.service';
 
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
-import BoardUser from "./components/BoardUser";
-import BoardModerator from "./components/BoardModerator";
-import BoardAdmin from "./components/BoardAdmin";
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import BoardUser from './components/BoardUser';
+import BoardModerator from './components/BoardModerator';
+import BoardAdmin from './components/BoardAdmin';
 
 // import AuthVerify from "./common/AuthVerify";
-import EventBus from "./common/EventBus";
+import EventBus from './common/EventBus';
 
 const App = () => {
   const [showModeratorBoard, setShowModeratorBoard] = useState(false);
   const [showAdminBoard, setShowAdminBoard] = useState(false);
   const [currentUser, setCurrentUser] = useState(undefined);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const user = AuthService.getCurrentUser();
-
+    if (!user) {
+      navigate('/login');
+    }
+    console.log('user', user);
     if (user) {
       setCurrentUser(user);
-      setShowModeratorBoard(user.roles.includes("ROLE_MODERATOR"));
-      setShowAdminBoard(user.roles.includes("ROLE_ADMIN"));
+      setShowModeratorBoard(user.roles.includes('ROLE_MODERATOR'));
+      setShowAdminBoard(user.roles.includes('ROLE_ADMIN'));
     }
 
-    EventBus.on("logout", () => {
+    EventBus.on('logout', () => {
       logOut();
     });
 
     return () => {
-      EventBus.remove("logout");
+      EventBus.remove('logout');
     };
   }, []);
 
@@ -46,22 +49,27 @@ const App = () => {
     setCurrentUser(undefined);
   };
 
+  // if (!currentUser) {
+  //   return <Navigate to="/login" />;
+  // }
+
   return (
     <div>
+      {/* <nav style={{ display: 'none' }} className="navbar navbar-expand navbar-dark bg-dark"> */}
       <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <Link to={"/"} className="navbar-brand">
+        <Link to={'/'} className="navbar-brand">
           bezKoder
         </Link>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
-            <Link to={"/home"} className="nav-link">
+            <Link to={'/home'} className="nav-link">
               Home
             </Link>
           </li>
 
           {showModeratorBoard && (
             <li className="nav-item">
-              <Link to={"/mod"} className="nav-link">
+              <Link to={'/mod'} className="nav-link">
                 Moderator Board
               </Link>
             </li>
@@ -69,7 +77,7 @@ const App = () => {
 
           {showAdminBoard && (
             <li className="nav-item">
-              <Link to={"/admin"} className="nav-link">
+              <Link to={'/admin'} className="nav-link">
                 Admin Board
               </Link>
             </li>
@@ -77,7 +85,7 @@ const App = () => {
 
           {currentUser && (
             <li className="nav-item">
-              <Link to={"/user"} className="nav-link">
+              <Link to={'/user'} className="nav-link">
                 User
               </Link>
             </li>
@@ -87,7 +95,7 @@ const App = () => {
         {currentUser ? (
           <div className="navbar-nav ml-auto">
             <li className="nav-item">
-              <Link to={"/profile"} className="nav-link">
+              <Link to={'/profile'} className="nav-link">
                 {currentUser.username}
               </Link>
             </li>
@@ -100,13 +108,13 @@ const App = () => {
         ) : (
           <div className="navbar-nav ml-auto">
             <li className="nav-item">
-              <Link to={"/login"} className="nav-link">
+              <Link to={'/login'} className="nav-link">
                 Login
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link to={"/register"} className="nav-link">
+              <Link to={'/register'} className="nav-link">
                 Sign Up
               </Link>
             </li>
@@ -116,8 +124,8 @@ const App = () => {
 
       <div className="container mt-3">
         <Routes>
-          <Route exact path={"/"} element={<Home />} />
-          <Route exact path={"/home"} element={<Home />} />
+          <Route exact path={'/'} element={<Home />} />
+          <Route exact path={'/home'} element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/profile" element={<Profile />} />
--- src/components/BoardAdmin.js
+++ src/components/BoardAdmin.js
@@ -1,30 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import UserService from "../services/user.service";
-import EventBus from "../common/EventBus";
+import UserService from '../services/user.service';
+import EventBus from '../common/EventBus';
 
 const BoardAdmin = () => {
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState('');
 
   useEffect(() => {
     UserService.getAdminBoard().then(
       (response) => {
+        console.log(response);
         setContent(response.data);
       },
       (error) => {
         const _content =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
+          (error.response && error.response.data && error.response.data.message) ||
           error.message ||
           error.toString();
 
         setContent(_content);
 
         if (error.response && error.response.status === 401) {
-          EventBus.dispatch("logout");
+          EventBus.dispatch('logout');
         }
-      }
+      },
     );
   }, []);
 
